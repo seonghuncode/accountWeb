d29d47abb67fb676b70cef8dca4c2ff137536979
seonghuncode <gnos456@naver.com>
2023-02-08 17:04:01 +0900

    프로젝트 구조 수정(web위치를 src,main안으로 이동)

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,10 +9,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
 
 
-    @RequestMapping(value="/")
-    public String test(){
-        return "jsp/test/index";
-    }
+//    @RequestMapping(value="/")
+//    public String test(){
+//        return "jsp/test/index";
+//    }
 
     @RequestMapping(value = "/test", produces = "application/json; charset=utf8")
     @ResponseBody
@@ -23,5 +24,16 @@ public class TestController {
     public String test3(){
         return "jsp/test/test3";
     }
+
+    //타임리프
+    @RequestMapping(value ="/usr/test")
+    public String doJoin(Model model){
+        model.addAttribute("greeting", "Success입니다.");
+        return "thymeleaf/test/test3";
+    }
+
+
+
+
 
 }
--- src/main/java/controller/UsrController.java
+++ src/main/java/controller/UsrController.java
@@ -8,14 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UsrController {
 
-    @RequestMapping(value ="/usr/login")
-    public String doJoin(Model model){
-        model.addAttribute("greeting", "Success입니다.");
-        return "thymeleaf/test3";
+    @RequestMapping(value = "/")
+    public String Main(){
+        return "redirect:/usr/login";
     }
 
 
-
+    @RequestMapping(value = "/usr/login")
+    public String doJoin(Model model) {
+        return "thymeleaf/usr/login";
+    }
 
 
 }
