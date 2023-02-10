2581b1531c8e22590330859a4da225f774a930d0
seonghuncode <gnos456@naver.com>
2023-02-10 17:57:29 +0900

    회원가입 디자인 완료(기능X)

--- src/main/java/controller/UsrController.java
+++ src/main/java/controller/UsrController.java
@@ -2,22 +2,45 @@ package controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrController {
 
+
+
+
+
+
     @RequestMapping(value = "/")
-    public String Main(){
-        return "redirect:/usr/login";
+    public String Main() {
+        return "redirect:/usr/loginForm";
     }
 
 
-    @RequestMapping(value = "/usr/login")
-    public String doJoin(Model model) {
+    //로그인 폼 화면으로 이동하는 코드
+    @RequestMapping(value = "/usr/loginForm")
+    public String showLogin() {
         return "thymeleaf/usr/login";
     }
 
 
+    //로그인 폼 에서 정보가 오면 해당 값에 대해 예외처리 및 결과 값 코드 작성 + 성공시 세션 설정
+    @RequestMapping(value = "/usr/loginFn", produces = "application/json; charset=utf8")
+    @ResponseBody
+    public String doLogin(String loginId, String loginPw) {
+        return "로그인";
+    }
+
+
+    @RequestMapping("/usr/joinForm")
+    public String showJoin() {
+        return "thymeleaf/usr/join";
+    }
+
+    @RequestMapping("/usr/joinFn")
+    public String doJoin() {
+        return "thymeleaf/usr/join";
+    }
 }
